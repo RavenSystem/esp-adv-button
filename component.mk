6408b9077b271e2a7b021c329927f58ba6350760
José Antonio Jiménez <RavenSystem@users.noreply.github.com>
2019-08-11 17:36:49 +0200

    Update component.mk

--- component.mk
+++ component.mk
@@ -2,7 +2,7 @@
 
 INC_DIRS += $(esp_adv_button_ROOT)
 
-adv_button_INC_DIR = $(esp_adv_button_ROOT)
-adv_button_SRC_DIR = $(esp_adv_button_ROOT)
+esp_adv_button_INC_DIR = $(esp_adv_button_ROOT)
+esp_adv_button_SRC_DIR = $(esp_adv_button_ROOT)
 
 $(eval $(call component_compile_rules,esp_adv_button))
